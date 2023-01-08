917468786ccf295cd6b369a250c61a4d106db64a
Piyush Acharya <acharyapiyush1@outlook.com>
2023-01-07 23:20:44 -0800

    Fix about team formatting

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,78 +1,100 @@
-<h1>About Altruity</h1>
-<p class="lead">Altruity is a revolutionary social media platform connecting philanthropists and charities, making it easy to discover and support causes that matter to you.</p>
+<h1>What is Altruity</h1>
+<p>
+    Altruity is a revolutionary social media platform connecting philanthropists
+    and charities, making it easy to discover and support causes that matter to
+    you.
+</p>
+<p>
+    Altruity is a social media platform that connects charitable organizations
+    with potential donors and volunteers. With a user-friendly interface and
+    powerful tools, Altruity makes it easy for charities to share their
+    missions, fundraise, and connect with their communities. Whether you're a
+    charity looking to spread the word about your cause or a philanthropist
+    searching for ways to make a difference, Altruity has something for you.
+    With features like customizable profiles, event listings, and easy-to-use
+    donation buttons, Altruity streamlines the process of connecting charities
+    with the resources they need to make an impact. Join the Altruity community
+    today and start making a difference!
+</p>
 
-<h2>Our Team</h2>
+<h1>Our Team</h1>
 <div class="row">
-  <div class="col">
-    <div class="card">
-      <!--<img src="/w3images/team1.jpg" alt="Aarnav" style="width:100%" class="card-img-top"/>-->
-      <div class="card-body">
-        <h5 class="card-title">Aarnav Bhat</h5>
-        <p class="card-text">Researcher</p>
-        <p class="card-text">
-          Aarnav Bhat is a freshman at Interlake High School. He is passionate
-          about Computer Science and Business and helps his local community by
-          refereeing games and tutoring kids via Schoohouse.world.
-        </p>
-        <a
-          href="https://www.piyushacharya.com"
-          target="_blank" rel="noreferrer"
-          class="btn btn-primary">Website</a
-        >
-      </div>
+    <div class="col">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Aarnav Bhat</h5>
+                <p class="card-text">Role</p>
+                <p class="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+                <a
+                    href="https://www.piyushacharya.com"
+                    target="_blank"
+                    rel="noreferrer"
+                    class="btn btn-primary">Website</a
+                >
+            </div>
+        </div>
     </div>
-  </div>
 
-  <div class="col">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Piyush Acharya</h5>
-        <p class="card-text">Researcher</p>
-        <p class="card-text">
-          Piyush Acharya is a freshman at Interlake High School interested in
-          SWE. He recently placed first at the U.S. Department of Energy's <a
-            href="https://www.energy.gov/articles/doe-announces-winners-32nd-annual-national-science-bowl#:~:text=include%3A%20Vishnu%20Mangipudi%2C-,Piyush%20Acharya,-%2C%20Aishwarya%20Agrawal%2C%20Aryan"
-            target="_blank" rel="noreferrer">National Science Bowl Finals</a
-          >. He also founded
-          <a
-            href="https://github.com/Verisimilitude11/DNAnalyzer"
-            target="_blank" rel="noreferrer">DNAnalyzer</a
-          >, an open source project that analyzes DNA sequences, where he manages
-          a team of over 30 contributors. In addition to his passion for science
-          and technology, he is deeply committed to giving back to his
-          community, and he is fulfilling this as Executive Director of
-          <a href="https://www.mightycrayon.org/" target="_blank" rel="noreferrer"
-            >Mighty Crayon</a
-          >.
-        </p>
-        <a
-          href="https://www.piyushacharya.com"
-          target="_blank" rel="noreferrer"
-          class="btn btn-primary">Website</a
-        >
-      </div>
+    <div class="col">
+        <div class="card">
+            <!--<img src="/img/team/piyush.png" alt="Piyush" style="width: 100%" class="card-img-top"/>-->
+            <div class="card-body">
+                <h5 class="card-title">Piyush Acharya</h5>
+                <p class="card-text">Role</p>
+                <p class="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+                <a
+                    href="https://www.piyushacharya.com"
+                    target="_blank"
+                    rel="noreferrer"
+                    class="btn btn-primary">Website</a
+                >
+            </div>
+        </div>
     </div>
-  </div>
 
-  <div class="col">
-    <div class="card">
-      <!--<img src="/w3images/team3.jpg" alt="Nishant" style="width:100%" class="card-img-top"/>-->
-      <div class="card-body">
-        <h5 class="card-title">Nishant Vikramaditya</h5>
-        <p class="card-text">Lead Developer</p>
-        <p class="card-text">
-          Nishant Vikramaditya is a freshman at Interlake High School. He is a
-          passionate developer who enjoys backend development and database
-          architecture. On his <a href="https://nv7haven.com" target="_blank" rel="noreferrer"
-            >website</a
-          >, he has a variety of games and projects showcasing his interest in
-          programming. He is also the lead developer for Elemental on Discord,
-          which handles over 500 commands per day. In his spare time, Nishant
-          enjoys developing Discord bots and experimenting with various
-          programming languages.
-        </p>
-        <a href="https://nv7haven.com" class="btn btn-primary">Website</a>
-      </div>
+    <div class="col">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Nishant Vikramaditya</h5>
+                <p class="card-text">Role</p>
+                <p class="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+                <a href="https://nv7haven.com" class="btn btn-primary"
+                    >Website</a
+                >
+            </div>
+        </div>
     </div>
-  </div>
+    <div class="col">
+        <div class="card">
+            <!--<img src="/w3images/team3.jpg" alt="Nishant" style="width:100%" class="card-img-top"/>-->
+            <div class="card-body">
+                <h5 class="card-title">Aryan Agrawal</h5>
+                <p class="card-text">Role</p>
+                <p class="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+                <a href="https://nv7haven.com" class="btn btn-primary"
+                    >Website</a
+                >
+            </div>
+        </div>
+    </div>
 </div>
