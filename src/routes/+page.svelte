738800f3ad9115d400f5debeab1c12511df1b4cb
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2023-01-07 20:21:46 -0800

    Make homepage look nice

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,1 @@
-<h1>Altruity</h1>
-<p>The best place for charities, volunteers and donors. </p>
+Feed
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,2 @@
+<h1>Altruity</h1>
+<p class="lead">The best place for charities, volunteers and donors. </p>
