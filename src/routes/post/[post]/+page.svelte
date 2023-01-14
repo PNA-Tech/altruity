d452ea1b7f2304e45e083366d68f83c1dfa2885c
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2023-01-14 15:34:22 -0800

    Fix up post page

--- src/routes/post/[post]/+page.svelte
+++ src/routes/post/[post]/+page.svelte
@@ -10,15 +10,32 @@
   let userLiked: boolean;
   $: userLiked = likes.some((v) => v.user == $user?.id)
 
+  let comments: Record[] = [];
+  let comment: string;
+
+  let commenting = false;
+  async function makeComment() {
+    commenting = true;
+    await pb.collection("comments").create({ post: post.id, author: $user?.id, comment });
+    comment = "";
+    commenting = false;
+  }
+
+  function asRecord(v: Record | Record[]): Record {
+    return v as Record;
+  }
+
   let post: Record;
   let author: Record;
-  let unsubscribe: () => void;
+  let unsubscribeLikes: () => void;
+  let unsubscribeComments: () => void;
   onMount(async () => {
     post = await pb.collection("posts").getOne($page.params.post, { expand: "author" });
     author = post.expand.author as Record;
     likes = await pb.collection("likes").getFullList(200, { filter: `post="${post.id}"` });
+    comments = await pb.collection("comments").getFullList(200, { filter: `post="${post.id}"`, expand: "author" });
 
-    unsubscribe = await pb.collection("likes").subscribe("*", async (rec) => {
+    unsubscribeLikes = await pb.collection("likes").subscribe("*", async (rec) => {
       if (rec.record.post != post.id) {return;}
       if (rec.action == "create") {
         likes = [...likes, rec.record];
@@ -27,11 +44,24 @@
       }
     })
 
+    unsubscribeComments = await pb.collection("comments").subscribe("*", async (rec) => {
+      if (rec.record.post != post.id) {return;}
+      if (rec.action == "create") {
+        // Expand author
+        let author = await pb.collection("users").getOne(rec.record.author);
+        rec.record.expand = { author };
+        comments = [...comments, rec.record];
+      } else if (rec.action == "delete") {
+        comments = comments.filter((l) => l.id != rec.record.id);
+      }
+    })
+
     loaded = true;
   })
 
   onDestroy(() => {
-    if (unsubscribe) {unsubscribe()};
+    if (unsubscribeLikes) {unsubscribeLikes()};
+    if (unsubscribeComments) {unsubscribeComments()};
   })
 
   let liking = false;
@@ -52,7 +82,7 @@
   <div class="row mb-3 align-items-center">
     <img src={pb.getFileUrl(author, author.avatar, { thumb: "50x50" })} alt={author.username} class="rounded-circle avatar col-2 img-fluid"/>
     <div class="col">
-      <h2 class="row">{author.username}</h2>
+      <a href={"/user/" + author.id} class="row h2 usertext">{author.username}</a>
       <span class="text-muted row">Posted on {post.created}</span>
     </div>
   </div>
@@ -67,13 +97,36 @@
   {/if}
 
   {#if $user}
-  <div class="btn-group btn-group-lg w-100">
-    <button class="btn" class:btn-danger={userLiked} class:btn-success={!userLiked} on:click={like} disabled={liking}>
-      <i class="bi bi-hand-thumbs-up-fill"></i>
-      {likes.length} Like{#if likes.length != 1}s{/if} | {userLiked ? "Unlike" : "Like"}
+  <div class="row justify-content-end mb-3">
+    <span class="col text-center">{likes.length} Like{#if likes.length != 1}s{/if}</span>
+    <button class="btn col btn-light" on:click={like} disabled={liking}>
+      <i class={"bi bi-hand-thumbs-up" + (userLiked ? "-fill" : "")}></i>
     </button>
   </div>
+
+  <form class="input-group w-100 mb-3">
+    <input type="text" class="form-control" bind:value={comment} placeholder="Post a comment..."/>
+    <button class="btn btn-primary" on:click|preventDefault={makeComment} disabled={commenting}>Post</button>
+  </form>
   {/if}
+
+  <div class="col mb-3">
+    {#each comments as comment}
+      <div class="row mb-3 align-items-center">
+        <img src={pb.getFileUrl(author, asRecord(comment.expand.author).avatar, { thumb: "50x50" })} alt={author.username} class="rounded-circle comment-avatar col-2 img-fluid"/>
+        <div class="col">
+          <a class="row h5 usertext mb-0" href={"/user/" + asRecord(comment.expand.author).id}>{asRecord(comment.expand.author).username}</a>
+          <span class="row">{comment.comment}</span>
+        </div>
+      </div>
+    {/each}
+  </div>
+</div>
+{:else}
+<div class="d-flex justify-content-center">
+  <div class="spinner-border" role="status">
+    <span class="visually-hidden">Loading...</span>
+  </div>
 </div>
 {/if}
 
@@ -89,5 +142,13 @@
 
   .avatar {
     width: 90px;
+  }
+
+  .comment-avatar {
+    width: 65px;
+  }
+
+  .usertext {
+    text-decoration: none;
   }
 </style>
