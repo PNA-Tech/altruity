118be79c03d35f1e4b390b0e638693ca221da612
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2023-01-16 11:51:26 -0800

    Add about input

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -3,6 +3,7 @@
 
   let username = $user!.username;
   let avatar: FileList;
+  let about = $user!.about ?? "";
   let donationUrl = $user!.donate ?? "";
   let topic = $user!.topic ?? "";
 
@@ -12,6 +13,9 @@
 
     let data = new FormData();
     data.append("username", username);
+    if (about != "") {
+      data.append("about", about);
+    }
     if ($user!.kind == "charity") {
       data.append("donate", donationUrl);
       data.append("topic", topic);
@@ -41,16 +45,20 @@
   </div>
   <div class="mb-3 text-start">
     <label for="avatar" class="form-label">Avatar</label>
-    <input type="file" class="form-control" id="avatar" bind:files={avatar} accept="image/jpg, image/jpeg, image/png">
+    <input type="file" class="form-control" id="avatar" bind:files={avatar} accept="image/jpg, image/jpeg, image/png" />
+  </div>
+  <div class="mb-3 text-start">
+    <label for="about" class="form-label">About</label>
+    <textarea class="form-control" id="about" bind:value={about} rows="3" placeholder="I love philanthropy!"></textarea>
   </div>
   {#if $user?.kind == "charity"}
   <div class="mb-3 text-start">
     <label for="donation" class="form-label">Donation Link</label>
-    <input type="url" class="form-control" id="donation" bind:value={donationUrl} placeholder="Donation website URL">
+    <input type="url" class="form-control" id="donation" bind:value={donationUrl} placeholder="Donation website URL" />
   </div>
   <div class="mb-3 text-start">
     <label for="donation" class="form-label">Topic</label>
-    <input type="topic" class="form-control" id="topic" bind:value={topic} placeholder="e.g. Crayons">
+    <input type="topic" class="form-control" id="topic" bind:value={topic} placeholder="e.g. Crayons" />
   </div>
   {/if}
   <button type="submit" class="btn btn-primary btn-lg w-100" disabled={loading} on:click|preventDefault={edit}>Save</button>
