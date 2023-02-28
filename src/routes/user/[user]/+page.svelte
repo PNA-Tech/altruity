5c09748ed83d3bc0b3776ac95d75be19360b6ae7
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2023-02-27 19:55:14 -0800

    Clean it up

--- src/routes/user/[user]/+page.svelte
+++ src/routes/user/[user]/+page.svelte
@@ -9,7 +9,6 @@
   import { onDestroy } from "svelte";
   import type { PageData } from "./$types";
 
-
   export let data: PageData;
 
   let user = data.user;
@@ -30,35 +29,59 @@
     loaded = false;
 
     // Get posts
-    posts = (await pb.collection("posts").getList(1, 50, { filter: `author="${user.id}"`, sort: "-created", expand: "author" })).items;
-    unsubscribePosts = await pb.collection("posts").subscribe("*", async (rec) => {
-      if (rec.record.author != user.id) {return;}
-      if (rec.action == "create") {
-        // Expand author
-        let author = await pb.collection("users").getOne(rec.record.author);
-        rec.record.expand = { author };
-        posts = [rec.record, ...posts];
-      } else if (rec.action == "delete") {
-        posts = posts.filter((l) => l.id != rec.record.id);
-      }
-    })
+    posts = (
+      await pb.collection("posts").getList(1, 50, {
+        filter: `author="${user.id}"`,
+        sort: "-created",
+        expand: "author",
+      })
+    ).items;
+    unsubscribePosts = await pb
+      .collection("posts")
+      .subscribe("*", async (rec) => {
+        if (rec.record.author != user.id) {
+          return;
+        }
+        if (rec.action == "create") {
+          // Expand author
+          let author = await pb.collection("users").getOne(rec.record.author);
+          rec.record.expand = { author };
+          posts = [rec.record, ...posts];
+        } else if (rec.action == "delete") {
+          posts = posts.filter((l) => l.id != rec.record.id);
+        }
+      });
 
     // Follower/following count
     followingUsers = (await pb.collection("users").getOne(user.id)).following;
-    unsubscribeFollowerCount = await pb.collection("users").subscribe(user.id, async (rec) => {
-      if (rec.action == "update") {
-        followingUsers = rec.record.following;
-      }
-    })
-    followerUsers = (await pb.collection("users").getFullList(50, { filter: `following.id ?= "${user.id}"` })).map((l) => l.id);
-    unsubscribeFollowingCount = await pb.collection("users").subscribe("*", async (rec) => {
-      if (!followerUsers.includes(rec.record.id) && rec.record.following.includes(user.id)) {
-        followerUsers = [...followerUsers, rec.record.id];
-      }
-      if (followerUsers.includes(rec.record.id) && !rec.record.following.includes(user.id)) {
-        followerUsers = followerUsers.filter((l) => l != rec.record.id);
-      }
-    })
+    unsubscribeFollowerCount = await pb
+      .collection("users")
+      .subscribe(user.id, async (rec) => {
+        if (rec.action == "update") {
+          followingUsers = rec.record.following;
+        }
+      });
+    followerUsers = (
+      await pb
+        .collection("users")
+        .getFullList(50, { filter: `following.id ?= "${user.id}"` })
+    ).map((l) => l.id);
+    unsubscribeFollowingCount = await pb
+      .collection("users")
+      .subscribe("*", async (rec) => {
+        if (
+          !followerUsers.includes(rec.record.id) &&
+          rec.record.following.includes(user.id)
+        ) {
+          followerUsers = [...followerUsers, rec.record.id];
+        }
+        if (
+          followerUsers.includes(rec.record.id) &&
+          !rec.record.following.includes(user.id)
+        ) {
+          followerUsers = followerUsers.filter((l) => l != rec.record.id);
+        }
+      });
 
     loaded = true;
   }
@@ -67,30 +90,43 @@
     if (loaded && v.params.user != user.id) {
       load();
     }
-  })
+  });
 
-  onMount(load)
+  onMount(load);
 
   let followLoading = false;
   async function follow() {
     followLoading = true;
     if (followerUsers.includes($loggedUser!.id)) {
-      await pb.collection("users").update($loggedUser!.id, { following: $loggedUser!.following.filter((l: string) => l != user.id) })
+      await pb.collection("users").update($loggedUser!.id, {
+        following: $loggedUser!.following.filter((l: string) => l != user.id),
+      });
     } else {
-      await pb.collection("users").update($loggedUser!.id, { following: [...$loggedUser!.following, user.id] })
-      publishFeed({
-        kind: "follow",
-        author: $loggedUser!.id,
-        user: user.id,
-      }, $loggedUser!.id)
+      await pb.collection("users").update($loggedUser!.id, {
+        following: [...$loggedUser!.following, user.id],
+      });
+      publishFeed(
+        {
+          kind: "follow",
+          author: $loggedUser!.id,
+          user: user.id,
+        },
+        $loggedUser!.id
+      );
     }
     followLoading = false;
   }
 
   function unload() {
-    if (unsubscribePosts) {unsubscribePosts()};
-    if (unsubscribeFollowerCount) {unsubscribeFollowerCount()};
-    if (unsubscribeFollowingCount) {unsubscribeFollowingCount()};
+    if (unsubscribePosts) {
+      unsubscribePosts();
+    }
+    if (unsubscribeFollowerCount) {
+      unsubscribeFollowerCount();
+    }
+    if (unsubscribeFollowingCount) {
+      unsubscribeFollowingCount();
+    }
   }
 
   onDestroy(unload);
@@ -101,7 +137,11 @@
 </svelte:head>
 
 <div class="row mb-3 align-items-center">
-  <img src={pb.getFileUrl(user, user.avatar, { thumb: "128x128" })} alt={user.username} class="rounded-circle avatar col-3 img-fluid avatar"/>
+  <img
+    src={pb.getFileUrl(user, user.avatar, { thumb: "128x128" })}
+    alt={user.username}
+    class="rounded-circle avatar col-3 img-fluid avatar"
+  />
   <div class="col">
     <h1 class="row">{user.username}</h1>
     <span class="text-muted row">Joined {formatTime(user.created)}</span>
@@ -116,46 +156,57 @@
     <div class="col-6 text-muted">{followingUsers.length}</div>
   </div>
   {#if $loggedUser && $loggedUser.id != user.id}
-  <button class="col-3 btn btn-primary me-2" disabled={followLoading} on:click={follow}>{followerUsers.includes(asRecord($loggedUser).id) ? "Unfollow" : "Follow"}</button>
+    <button
+      class="col-3 btn btn-primary me-2"
+      disabled={followLoading}
+      on:click={follow}
+      >{followerUsers.includes(asRecord($loggedUser).id)
+        ? "Unfollow"
+        : "Follow"}</button
+    >
   {:else if $loggedUser && $loggedUser.id == user.id}
-  <a class="col-3 btn btn-outline-primary me-2" href="/settings">
-    <div class="h-100 lgbtn">
-      <span class="d-flex"><i class="bi bi-pencil-square me-2"></i>Edit</span>
-    </div>
-  </a>
+    <a class="col-3 btn btn-outline-primary me-2" href="/settings">
+      <div class="h-100 lgbtn">
+        <span class="d-flex"><i class="bi bi-pencil-square me-2" />Edit</span>
+      </div>
+    </a>
   {/if}
 </div>
 
 {#if user.about && user.about.length > 0}
-<p class="lead mb-3">
-  {user.about}
-</p>
+  <p class="lead mb-3">
+    {user.about}
+  </p>
 {/if}
 
 {#if user.kind == "nonprofit"}
-<div class="row mb-3 align-items-center">
-  <div class="d-flex align-items-center">
-    <a class="btn btn-primary mr-2" target="_blank" rel="noreferrer" href={user.donate} style="width: 30%; height:80%">
-      Donate 
+  <div class="row mb-3 align-items-center">
+    <a
+      class="btn btn-primary me-2 btn-lg col donatebtn"
+      target="_blank"
+      rel="noreferrer"
+      href={user.donate}
+    >
+      <i class="bi bi-bag-heart" />
+      Donate
     </a>
-    <!-- Ignore error message(acts as space to fill up gap) -->
-    <a style="padding-left: 32%;"></a>
-    <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" class="bi bi-tags" viewBox="0 1 16 16">
-      <path d="M3 2v4.586l7 7L14.586 9l-7-7H3zM2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586V2z"/>
-      <path d="M5.5 5a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm0 1a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zM1 7.086a1 1 0 0 0 .293.707L8.75 15.25l-.043.043a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 0 7.586V3a1 1 0 0 1 1-1v5.086z"/>
-    </svg>
-    <h5 style="font-size: large;padding-left:0.3%" id="user-topic">{user.topic}</h5>
-  </div>
-</div>
-{/if}
 
+    <div class="col">
+      <h5 class="topic">
+        <i class="bi bi-tags" />
+        {user.topic}
+      </h5>
+    </div>
+  </div>
+{/if}
+<br />
 <h1>Recent Activity</h1>
 {#each posts as post}
-  <PostPreview post={post}></PostPreview>
+  <PostPreview {post} />
 {/each}
 
 {#if !loaded}
-<Loading></Loading>
+  <Loading />
 {/if}
 
 <style>
@@ -168,9 +219,17 @@
     align-items: center;
     justify-content: center;
   }
-  #user-topic {
+
+  .topic {
     font-size: medium;
+    font-size: large;
+    padding-left: 0.3%;
   }
+
+  .donatebtn {
+    max-width: 400px;
+  }
+
   i {
     font-size: 1.2rem;
   }
